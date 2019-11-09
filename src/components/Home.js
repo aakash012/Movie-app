6b33eb19d324b116be3d64b7f1f8936e78fe2936
aakash012 <aakash012@live.com>
2019-11-09 23:07:48 +0530

    Movie info component added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
-  API_URL,
-  API_KEY,
-  API_BASE_URL,
+  POPULAR_BASE_URL,
+  SEARCH_BASE_URL,
   POSTER_SIZE,
   BACKDROP_SIZE,
   IMAGE_BASE_URL,
@@ -19,26 +18,69 @@ import Spinner from './elements/Spinner';
 // Custom Hook
 import { useHomeFetch } from './hooks/useHomeFetch';
 
-const Home = () => {
-  const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+import NoImage from './images/no_image.jpg';
 
-  console.log(state);
+const Home = () => {
+  const [
+    {
+      state: { movies, currentPage, totalPages, heroImage },
+      loading,
+      error,
+    },
+    fetchMovies,
+  ] = useHomeFetch();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchMovies = search => {
+    const endpoint = search ? SEARCH_BASE_URL + search : POPULAR_BASE_URL;
+
+    setSearchTerm(search);
+    fetchMovies(endpoint);
+
+  }
+
+  const loadMoreMovies = () => {
+    const searchEndpoint = `${SEARCH_BASE_URL}${searchTerm}&page=${currentPage + 1}`;
+    const popularEndpoint = `${POPULAR_BASE_URL}&page=${currentPage + 1}`;
+
+    const endpoint = searchTerm ? searchEndpoint : popularEndpoint;
+
+    fetchMovies(endpoint);
+
+  }
 
   if (error) return <div>Something went wrong ...</div>;
-  if (!state.movies[0]) return <Spinner />;
+  if (!movies[0]) return <Spinner />;
 
   return (
     <>
-      <HeroImage
-        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
-        title={state.heroImage.original_title}
-        text={state.heroImage.overview}
-      />
-      <SearchBar />
-      <Grid />
-      <MovieThumb />
-      <Spinner />
-      <LoadMoreBtn />
+      {!searchTerm && (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
+          title={heroImage.original_title}
+          text={heroImage.overview}
+        />
+      )}
+      <SearchBar callback={searchMovies} />
+      <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
+        {movies.map(movie => (
+          <MovieThumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+            movieId={movie.id}
+            movieName={movie.original_title}
+          />
+        ))}
+      </Grid>
+      {loading && <Spinner />}
+      {currentPage < totalPages && !loading && (
+        <LoadMoreBtn text="Load More" callback={loadMoreMovies} />
+      )}
     </>
   );
 };
