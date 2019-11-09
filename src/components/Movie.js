8ea73771f3484a4fd8526698543ce11029aeba2a
aakash012 <aakash012@live.com>
2019-11-09 23:11:53 +0530

    Movie fetch function added

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
 // Components
-import Navigation from './elements/Navigation.js';
+import Navigation from './elements/Navigation';
 import MovieInfo from './elements/MovieInfo';
 import MovieInfoBar from './elements/MovieInfoBar';
 import Actor from './elements/Actor';
 import Grid from './elements/Grid';
 import Spinner from './elements/Spinner';
 
-const Movie = ({ movieId }) => (
+import { useMovieFetch } from './hooks/useMovieFetch';
+
+const Movie = ({ movieId }) => {
+  const [movie, loading, error] = useMovieFetch(movieId);
+  console.log(movie);
+
+  return (
   <>
     <Navigation />
     <MovieInfo />
@@ -18,6 +24,7 @@ const Movie = ({ movieId }) => (
     </Grid>
     <Spinner />
   </>
-);
+  )
+};
 
 export default Movie;
